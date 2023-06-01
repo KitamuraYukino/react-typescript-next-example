05544836df995656792dfdd0f2f6e00f77eeb581
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-06-01 21:23:03 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,19 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+	return (
+		<>
+			<section>
+				<h1>
+				Learn <a href="https://nextjs.org/learn/foundations/about-nextjs?utm_source=next-site&utm_medium=nav-cta&utm_campaign=next-website">Next.js!</a>
+				</h1>
+				<p>Get started by editing <code>pages/index.js</code></p>
+			</section>
+		</>
+	);
+}
+
+/*export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -93,3 +106,4 @@ export default function Home() {
     </main>
   )
 }
+*/
